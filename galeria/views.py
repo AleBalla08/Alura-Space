ff9580e24c0d4a8899ed7f6797841027a4f25835
Alexandre Ballardin <aleballa08@gmail.com>
2024-08-29 15:55:28 -0300

    adicionando banco

--- galeria/views.py
+++ galeria/views.py
@@ -2,7 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'galeria/index.html')
+    dados = {
+        1: {'nome':'Pilares da criação',
+            'legenda':'Webbtelescope.org / NASA / James Webb',
+            'tag': 'Formações'},
+        2: {'nome':'Galáxia de Andromeda',
+            'legenda':'nasa.org / NASA / Hubble',
+            'tag':'Galáxias'}
+    }
+   
+    return render(request, 'galeria/index.html', {"cards": dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
